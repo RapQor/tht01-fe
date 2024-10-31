95b30c88d891318dc19d0244da516d842c2b7acb
RafiAbqor <rafi.abqor@gmail.com>
2024-10-31 11:17:43 +0700

    fix: api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://tht01-be.vercel.app",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 25000, // 25 seconds
+  baseURL: "https://tht01-be.vercel.app/",
 });
