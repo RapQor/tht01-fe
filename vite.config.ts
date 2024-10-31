c83d6ebfb9a7afe83cc27e5c87d15fcd2ce0de04
RafiAbqor <rafi.abqor@gmail.com>
2024-10-31 11:16:36 +0700

    fix: api

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://tht01-be.vercel.app",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
